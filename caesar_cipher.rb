80aebc1d5d53d0de0cdf9e499ed31b7b2a150702
cordelia <80014084+cordelia-sixth@users.noreply.github.com>
2022-05-29 01:41:11 +0900

    Update caesar_cipher.rb

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,15 +1,47 @@
+require "test/unit"
+
 def caesar_cipher(string = "ABC", shift = 3)
-  # 暗号化したい文字列を文字ごとの配列に変換してさらに文字コードに変換して配列にする
   charArray = string.chars.map(&:ord)
-  
-  result = []
-  result << charArray.map do |char|
+
+  charArray.map do |char|
     if (char + shift) < 91
       (char + shift).chr
     else
       (char - 26).chr
     end
   end.join
-  
-  return result
 end
+
+class CaeasarCipher
+  def encrypt(string, shift)
+    # 文字列とシフト数を受け取る
+    
+    # 文字列のコードポイントを取得する
+    code_points = string.codepoints
+    
+    # シフト数だけ文字をシフトする
+    code_points.map do |point|
+      if (point + shift) < "Z".code_points
+        (point + shift).chr
+      else
+        (point + shift - 26).chr
+      end
+    end.join
+    
+    # コードポイントを文字に戻す
+    # 文字列を返す
+  end
+end
+
+caesar = CaeasarCipher.new
+puts caesar.encrypt("XYZ", 3)
+
+class TestCaesar < Test::Unit::TestCase
+  test ""
+end
+
+# returnが不要
+# 配列ではなく文字列を返す方が自然
+# テストコードを書く
+# コードポイントを得るライブラリを使う
+# 変数名をsnake_caseに、かつ変数名をふさわしくする
